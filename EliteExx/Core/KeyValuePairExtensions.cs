6e164048f0cf3114b1f998a740bdef09d157dd57
Ricky Blankenaufulland <zoolway@gmail.com>
2019-07-01 22:28:01 +0200

    new: extension method to handle KeyValuePair destruction

---
+++ EliteExx/Core/KeyValuePairExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zw.EliteExx.Core
+{
+    /// <summary>
+    /// Extension methods for KeyValuePair.
+    /// </summary>
+    public static class KeyValuePairExtensions
+    {
+        /// <summary>
+        /// Tuple deconstructor.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="kvp"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp, out TKey key, out TValue value)
+        {
+            key = kvp.Key;
+            value = kvp.Value;
+        }
+    }
+}
