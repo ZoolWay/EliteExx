e1f16d8b6f6ad63a73b87185ab12eadecd5fc0b7
Ricky Blankenaufulland <zoolway@gmail.com>
2019-07-01 22:28:56 +0200

    new: extension for enumerable

---
+++ EliteExx/Core/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace Zw.EliteExx.Core
+{
+    public static class EnumerableExtensions
+    {
+        public static ImmutableArray<T> ToImmutableArrayOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null) return ImmutableArray<T>.Empty;
+            return enumerable.ToImmutableArray<T>();
+        }
+    }
+}
