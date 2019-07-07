e4db9051bab88006aed86eb4fb9aa9c1573e5fa4
Ricky Blankenaufulland <zoolway@gmail.com>
2019-07-08 00:44:32 +0200

    new: added CharExtensions

---
+++ EliteExx/Core/CharExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zw.EliteExx.Core
+{
+    public static class CharExtensions
+    {
+        public static bool IsDefaultOrWhitespace(this char c)
+        {
+            if (c == default(char)) return true;
+            return Char.IsWhiteSpace(c);
+        }
+    }
+}
