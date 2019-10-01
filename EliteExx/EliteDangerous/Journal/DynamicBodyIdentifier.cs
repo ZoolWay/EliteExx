7facb1ca9873db24ce8505912764120521fbcb9e
Ricky Blankenaufulland <zoolway@gmail.com>
2019-10-01 16:45:03 +0200

    fix: DynamicBodyIdentifier could not be deserialized because the constructor was missing

--- EliteExx/EliteDangerous/Journal/DynamicBodyIdentifier.cs
+++ EliteExx/EliteDangerous/Journal/DynamicBodyIdentifier.cs
@@ -2,11 +2,20 @@
 
 namespace Zw.EliteExx.EliteDangerous.Journal
 {
+    [System.Diagnostics.DebuggerDisplay("DynBodyId Null={Null}, Star={Star}, Planet={Planet}, Ring={Ring}")]
     public class DynamicBodyIdentifier
     {
         public long? Planet { get; }
         public long? Star { get; }
         public long? Null { get; }
         public long? Ring { get; }
+
+        public DynamicBodyIdentifier(long? planet, long? star, long? @null, long? ring)
+        {
+            this.Planet = planet;
+            this.Star = star;
+            this.Null = @null;
+            this.Ring = ring;
+        }
     }
 }
